b61febd51e4ed9ce68f674f45e48545236348170
tatrishvilikoba <kobatatrishvili21@gmail.com>
2024-08-30 14:34:36 +0400

    new

---
+++ level 10/homework/level 10 .py
@@ -0,0 +1,14 @@
+name = input("dad name") 
+print(name)
+name1 = input("mom name")
+print (name1)
+
+age = input("dad age")
+print (int(age)*int(age))
+age1 = input("mom age")
+print (int(age1)*int(age1))
+  
+time = input("ramdeni wlit?")
+print (int(time)+int(age))
+
+print (int(age)+ 25)
